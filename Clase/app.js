2de222a7bf370ae9b538de85c18671cda0f01484
Fernando Herrera <fernandoherrera@MacBook-Air-de-Luis.local>
2021-07-05 21:48:29 -0500

    Agregando

---
+++ Clase/app.js
@@ -0,0 +1,22 @@
+var x = 1;
+var y = "Hola Mundo";
+const PI = 3.1416;
+
+function suma(a,b)
+{
+    return a + b;
+}
+
+var section = document.getElementById("section");
+var h1 = document.getElementsByTagName("h1");
+var p = document.getElementsByClassName("p");
+var query = document.querySelectorAll(".p");
+
+console.log(x);
+console.log(y);
+console.log(PI);
+console.log(suma(3,5));
+console.log(section);
+console.log(h1);
+console.log(p);
+console.log(query);
